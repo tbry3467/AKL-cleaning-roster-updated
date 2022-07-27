6df3411a80dc304c5d4b16634000ff36b198ca6c
tbry3467 <tbry3467@users.noreply.github.com>
2022-07-26 19:51:56 -0500

    Updated code to reflect current chapter

--- README.md
+++ README.md
@@ -2,21 +2,20 @@
 
 ## Intro
   - Used by the Alpha Kappa Lambda, Alpha Zeta Chapter to randomly assign house cleaning jobs
-  - Originally coded in Java in 2020, re-coded in Python in Summer of 2021 with extended functionality
+  - Originally coded in Java in 2020, re-coded in Python with extended functionality
   - Author: Thomas Bryant - tbry3467 on GitHub
 
-## New Features
-  - Produces monthly output instead of weekly output
-  - Members will no longer get duplicate jobs several weeks in a row (no dupes in 1-month period)
+## New Features implemented
+  - Produces monthly output instead of weekly output (4 weeks = month)
+  - Members will no longer get duplicate jobs several weeks in a row (no dupes in 4-week period)
 
 ## Running the code
   - Download the Python file and run it using your IDE of choice
-  - input & output files included in the repo
+  - input file: akl_member_names.txt
+  - output file: test_output.xlsx
 
 ## How to Update for Future Use
   - akl_member_names.txt contains member names. Exclude new members because they clean the basement
-  - todo - adjusting job sizes
-
 
 ## Variables and Structures Explained
-  - todo, explain the stuff used in this code
+  - todo if I get around to it
--- akl_cleaning_roster_updated.py
+++ akl_cleaning_roster_updated.py
@@ -1,20 +1,22 @@
-# re-coded original house cleaning project in Python from Java. This is a WIP
 # Author: tbry3467 on GitHub
+# re-coded original Java house cleaning project in Python w/ added functionality
+# See the readme for objectives and requirements 
 # don't work too hard, fellas
+# todo: add a catch if all jobs are full, put everyone else in nojob
 
 from os import name
 import random
 from datetime import date
 from openpyxl import load_workbook
 
+
 class Person:
 
-    """ Used to regulate which members have been assigned what jobs.
-        each obj contains name and jobs1-4 represented as an int
+    """ Used to regulate which members have been assigned what jobs
+        each obj contains name and jobs1-4, represented as an int
         pass 0 for jobs1-4 when creating an obj, set later when assigned jobs
-        '5' would represent 'trash' has been assigned
+        ex: '5' would represent 'trash' has been assigned
     """
-
 
     def __init__(self, name, job1, job2, job3, job4): # can i just pass name and set jobs to 0 in constructor?
         self.name = name # note: single underscore signifies to readers that it's a private attribute
@@ -24,7 +26,9 @@ class Person:
         self.job4 = job4
 
 
+# todo: figure out the date when generating
 def get_mondays():
+    pass
     day = date.today 
     #d = today.strftime("%m/%d/%y") # mm/dd/y date format
     monday_dates = []
@@ -41,42 +45,39 @@ def get_mondays():
 
 
 # lists used to track num of workers per job 
-first_bathroom = []
-second_bathroom = []
-sanitize = []
-kitchen = []
-trash = []
-attic = []
-eagle_nest = []
-chapter_room = []
-odd_jobs = []
-no_job = []
-first_floor_sweep = []
-first_floor_swiffer = []
-second_floor_sweep = []
-second_floor_swiffer = []
-laundry_room = []
+first_bathroom = []         # capacity 2
+second_bathroom = []        # capacity 3
+kitchen = []                # capacity 4
+trash = []                  # capacity 1
+attic = []                  # capacity 3
+eagle_nest = []             # capacity 2
+chapter_room = []           # capacity 1
+odd_jobs = []               # capacity 5
+first_floor_sweep = []      # capacity 1
+first_floor_swiffer = []    # capacity 1
+second_floor_sweep = []     # capacity 1
+second_floor_swiffer = []   # capacity 1
+no_job = []                 # no capacity
 
-all_jobs = [first_bathroom, second_bathroom, sanitize, kitchen, trash, attic,
-eagle_nest, chapter_room, odd_jobs, no_job, first_floor_sweep, 
-first_floor_swiffer, second_floor_sweep, second_floor_swiffer, laundry_room]
 
-member_objects = []
-in_file = "test_names.txt" # "akl_member_names.txt" 
+# array of all jobs, used for random selection
+all_jobs = [first_bathroom, second_bathroom, kitchen, trash, attic,
+eagle_nest, chapter_room, odd_jobs, first_floor_sweep, 
+first_floor_swiffer, second_floor_sweep, second_floor_swiffer]
 
 
 # reading members and adding to list
-with open(in_file) as f:
-    content = f.read().splitlines() # chops off the NL char
+member_objects = []
+in_file = "akl_member_names.txt" # "akl_member_names.txt"
 
-for line in content: # pull names, create member objects and add to list
+with open(in_file) as f:
+    content = f.read().splitlines() # chops off NL char
+
+for line in content: # pull names, create member objects w default values and add to list
     line = Person(line,0,0,0,0)
     member_objects.append(line)
 
-# structure outline
-## check obj properties for duplicates when random rolls
-## loop member_objects, assign to lists & add int to member properties
-## clear job lists
+
 
 # main loop
 for x in range (1,5): # loops 4 times for 4 job assignments per person
@@ -84,8 +85,8 @@ for x in range (1,5): # loops 4 times for 4 job assignments per person
     for ppl in member_objects:
         # todo - assignment phase
         control = 0
-        job_nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14] # jobs mapped to num, remove num when job is full
-        job_sizes = [3,5,1,4,3,3,2,1,5,1,1,1,1,1] # num of ppl allowed in each job, in order
+        job_nums = [1,2,3,4,5,6,7,8,9,10,11,12] # jobs mapped to num, remove num when job is full
+        capacity = [2,3,4,1,3,2,1,5,1,1,1,1] # num of ppl allowed in each job, in order
 
         while control == 0:
             random_job = int(random.random() * len(job_nums)) # num btwn 1 and len of job_num, adjusts when nums get removed
@@ -96,9 +97,9 @@ for x in range (1,5): # loops 4 times for 4 job assignments per person
                 if job == random_job:
 
                     # check if job is at max capacity
-                    if len(all_jobs[random_job]) == job_sizes[random_job]: # if len(job) == capacity for said job:
+                    if len(all_jobs[random_job]) == capacity[random_job]: # if len(job) == capacity for said job:
                         job_nums.remove(job) # removes job num b/c full
-                        del job_sizes[job-1] # removes corresponding size for that job
+                        del capacity[job-1] # removes corresponding size for that job
                         continue
 
                     else: 
@@ -131,7 +132,7 @@ sheet = wb.worksheets[0]
 # clear cells B67 -> F67, this is where member names go
 
 # week 1 writing
-week_incrementer = 0
+week_incrementer = 0 # used to regulate cells being filled
 
 for col in range(2,6):
     for ppl in member_objects:
